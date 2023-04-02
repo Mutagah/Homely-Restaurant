8406214f105209113e53fef934cc517ac198d52c
Josphat-Kip <josphatkiplimo247@gmail.com>
2023-04-02 23:42:03 +0300

    modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_194256) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "password_confirmation"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
