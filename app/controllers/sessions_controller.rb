d23de7e4a566f8225ae064a14ebecf9029216ff6
Josphat-Kip <josphatkiplimo247@gmail.com>
2023-04-02 23:08:23 +0300

    modified sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,11 @@
 class SessionsController < ApplicationController
     def create
-      user = User.find_by(username: params[:username])
-  
-      if user&.authenticate(params[:password])
+      user = User.find_by(username: user_login_params[:username])
+      if user&.authenticate(user_login_params[:password])
         session[:user_id] = user.id
         render json: user, status: :created
       else
-        puts "DEBUG: Authentication failed for username #{params[:username]}"
+        puts "DEBUG: Authentication failed for username #{user_login_params[:username]}"
         render json: { errors: ["Invalid password or username"] }, status: :unauthorized
       end
     end
@@ -14,5 +13,10 @@ class SessionsController < ApplicationController
     def destroy
       session.delete(:user_id)
       head :no_content
+    end
+
+    private 
+    def user_login_params
+      params.permit(:username, :password)
     end
   end
